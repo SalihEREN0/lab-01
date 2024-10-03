f72312621593f7b57aa5f6353afa816731feba98
Salih Eren <saliheren266@gmail.com>
2024-10-03 18:29:57 +0300

    Reading the blob1 file
    a

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -11,12 +11,30 @@ using namespace std;
 int main()
 {
 	string filename;
+
 	cout << "Enter the filename: ";
-	cin >> filename; 
+	getline(cin, filename);
 
-	ifstream 
+	ifstream inFile;
+
+	inFile.open(filename);
+
+	if (inFile.fail())
+	{
+		cout << "Erorr while opening file!!!" << endl;
+		return 1;
+
+	}
+
+	char c;
+
+	while (inFile.get(c))
+	{
+		cout << c;
+	}
+
+	inFile.close();
 
 
-
-
+	return 0;
 }
