8933d1240023fd2170348f986ded7a44a0f00cb9
Salih Eren <saliheren266@gmail.com>
2024-10-03 17:33:18 +0300

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+	string filename;
+	cout << "Enter the filename: ";
+	cin >> filename; 
+
+	ifstream 
+
+
+
+
+}
